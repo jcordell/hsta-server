5b2c1f2ecb503a42f014ff90a77f740e0bf3997e
Chase <chhallchase@gmail.com>
2018-04-12 13:14:26 -0500

    implements ban_tournament_deck in api

--- routes/api.js
+++ routes/api.js
@@ -375,6 +375,23 @@ router.get('/add_tournament_deck', function(req, res){
     })
 });
 
+router.get('/ban_tournament_deck', function(req, res){
+    var userid = req.query.userid;
+    var tournamentid = req.query.tournamentid;
+    var deckcode = req.query.deckcode;
+    db_api.ban_tournament_deck(userid, tournamentid, deckcode, function(err, status){
+        if(err){
+            console.log("error db_api banning deck");
+            console.log(JSON.stringify(status));
+            res.send(JSON.stringify({success: false, error: err.message}));
+        }
+        else {
+            console.log(JSON.stringify(status));
+            res.send(JSON.stringify({success: true}));
+        }
+    })
+})
+
 /* input: params: matchid, winnerid
  * return: { 'success' : true/false, 'error' : none/error_code }*/
 router.get('/update_match_result', function(req, res) {
